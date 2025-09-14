448780b3524d515a8e8127a3fec0ce7ec308bf8e
KiritoGamesPLays <arthursantossousa05@gmail.com>
2025-09-13 22:28:30 -0300

    Mensagem da atualização

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const v2 = require("./v2.js");
-const v3 = require("./v3.js")
+const v3 = require("./v3.js");
+const v4 = require("./v4.js")
 
 const KiritoDB = v2.KiritoDB;
 const KiritoDBV3 = v3.KiritoDB;
+const KiritoDBV4 = v4.KiritoDB;
 
-module.exports = { KiritoDBV3, KiritoDB };
+module.exports = { KiritoDB, KiritoDBV3, KiritoDBV4 };
---
+++ v4.js
@@ -0,0 +1,142 @@
+client = require("./index");
+client.config = require("./config");
+const Urlbase = "https://npm-db-kiritodb.ecoguardiao.tech/kiritodb/v4"
+
+// Script by CroneGamesPlays Developer, NeoKurai Studios, ADS Sousa Group Corporation © 2020 - 2025 × Todos os direitos reservados.
+
+const axios = require('axios');
+
+class KiritoDB {
+  constructor(requiredString) {
+    if (!requiredString) {
+      throw new Error("Você deve informar uma key para poder proteger seus dados.");
+    }
+    this.requiredString = requiredString;
+  }
+
+  // 🔹 Ping
+  async ping() {
+    try {
+      let pingt = Date.now();
+      await axios.post(`${Urlbase}/api/v1/db/add`, {
+        id: "ping",
+        rota: "ping",
+        valor: 1
+      });
+      let pings = Date.now();
+      return pings - pingt;
+    } catch (error) {
+      throw new Error("Erro: " + error.message);
+    }
+  }
+
+  // 🔹 Add
+  async add(rota, valor) {
+    try {
+      if (!rota) throw new Error("Você Deve Informar a Rota.");
+      if (!valor || isNaN(valor) || !isFinite(valor)) {
+        throw new Error("Você Deve Informar Um Valor Numérico Válido");
+      }
+
+      const api = await axios.post(`${Urlbase}/api/v1/db/add`, {
+        id: this.requiredString,
+        rota,
+        valor
+      });
+
+      return api.data;
+    } catch (error) {
+      throw new Error("Erro: " + error.message);
+    }
+  }
+
+  // 🔹 Get
+  async get(rota) {
+    try {
+      if (!rota) throw new Error("Você Deve Informar a Rota.");
+      const api = await axios.post(`${Urlbase}/api/v1/db/get`, {
+        id: this.requiredString,
+        rota
+      });
+      return api.data;
+    } catch (error) {
+      throw new Error("Erro: " + error.message);
+    }
+  }
+
+  // 🔹 Sub
+  async sub(rota, valor) {
+    try {
+      if (!rota) throw new Error("Você Deve Informar a Rota.");
+      if (!valor) throw new Error("Você Deve Informar O Valor.");
+
+      const api = await axios.post(`${Urlbase}/api/v1/db/sub`, {
+        id: this.requiredString,
+        rota,
+        valor
+      });
+
+      return api.data;
+    } catch (error) {
+      throw new Error("Erro: " + error.message);
+    }
+  }
+
+  // 🔹 Delete
+  async delete(rota) {
+    try {
+      if (!rota) throw new Error("Você Deve Informar a Rota.");
+
+      const api = await axios.post(`${Urlbase}/api/v1/db/delete`, {
+        id: this.requiredString,
+        rota
+      });
+
+      return api.data;
+    } catch (error) {
+      throw new Error("Erro: " + error.message);
+    }
+  }
+
+  // 🔹 Set
+  async set(rota, valor) {
+    try {
+      if (!rota) throw new Error("Você Deve Informar a Rota.");
+      if (!valor) throw new Error("Você Deve Informar O Valor.");
+
+      const api = await axios.post(`${Urlbase}/api/v1/db/set`, {
+        id: this.requiredString,
+        rota,
+        valor
+      });
+
+      return api.data;
+    } catch (error) {
+      throw new Error("Erro: " + error.message);
+    }
+  }
+
+  // 🔹 All
+  async all() {
+    try {
+      const api = await axios.post(`${Urlbase}/api/v1/db/all`, {
+        id: this.requiredString
+      });
+      return api.data;
+    } catch (error) {
+      throw new Error("Erro: " + error.message);
+    }
+  }
+}
+
+module.exports = { KiritoDB };
+
+require("./update");
+
+process.on("uncaughtException", (err) => {
+  console.log("Exceção não capturada: " + err);
+});
+
+process.on("unhandledRejection", (reason, promise) => {
+  console.log("[GRAVE] Rejeição possivelmente não tratada em: Promise ", promise, " motivo: ", reason.message);
+});
